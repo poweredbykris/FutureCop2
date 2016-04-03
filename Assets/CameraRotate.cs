01275961a461a46befa0127d7b28d697622a14f0
kRIS <kris8360@gmail.com>
2016-04-03 22:00:00 +0200

    Upload

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour
+{
+
+    public GameObject what;
+    public int radius;
+    public float speed;
+
+    void Update()
+    {
+        gameObject.transform.LookAt(what.transform);
+        
+        transform.position = new Vector3(0 + radius * Mathf.Cos(Time.time * speed * 2 * 3.14f), 10, 0 + radius * Mathf.Sin(Time.time * speed * 2 * 3.14f));
+    }
+}
---
+++ Assets/FPS.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/* Kiírja hányszor rajzolódik ki egy kép másodpercenként (képkocka/másodperc)*/
+/* Unity oldaláról töltve, szabadon felhasználható, kisebb módosításokat végeztem rajta*/
+
+public class FPS : MonoBehaviour
+{
+    public float updateInterval = 0.5F;
+
+    private float accum = 0; // FPS accumulated over the interval
+    private int frames = 0; // Frames drawn over the interval
+    private float timeleft; // Left time for current interval
+
+    void Start()
+    {
+        timeleft = updateInterval;
+    }
+
+    void Update()
+    {
+       
+            PrintFPS();
+       
+    }
+
+    void PrintFPS()
+    {
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
+
+        // Interval ended - update GUI text and start new interval
+        if (timeleft <= 0.0)
+        {
+            float fps = accum / frames;
+            GetComponent<Text>().text = (int)fps + " FPS";
+
+            if (fps < 30)
+                GetComponent<Text>().material.color = Color.yellow;
+            else
+                if (fps < 10)
+                    GetComponent<Text>().material.color = Color.red;
+                else
+                    GetComponent<Text>().material.color = Color.green;
+
+            
+
+            timeleft = updateInterval;
+            accum = 0.0F;
+            frames = 0;
+        }
+
+    }
+
+   
+}
